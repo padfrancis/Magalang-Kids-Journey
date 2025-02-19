649c8d6b688ca87631e60ee335be7cd33b2dd1e6
Francis Padron <fbopadron@unp.edu.ph>
2025-02-20 00:18:44 +0800

    added UI for levels

---
+++ Assets/Scripts/LevelMenu.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class LevelMenu : MonoBehaviour
+{
+    public Button[] LevelButtons;
+    private void Awake()
+    {
+        int UnlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 1);
+        for (int i = 0; i < LevelButtons.Length; i++)
+        {
+            if (i + 1 > UnlockedLevel)
+            {
+                LevelButtons[i].interactable = false;
+            }
+        }
+    }
+    public void OpenLevel(int LevelId)
+    {
+        string levelName = "Level" + LevelId;
+        SceneManager.LoadScene(levelName);
+    }
+}
